98578f60491f35efdfc5b021b0b9edd8efce0407
jihwan.jeong <wnaks5945@gmail.com>
2024-05-10 17:00:59 +0900

    feat: login page updates

--- flutter_inflearn_practice/lib/main.dart
+++ flutter_inflearn_practice/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inflearn_practice/common/component/custom_text_form_field.dart';
+import 'package:flutter_inflearn_practice/user/view/login_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
       ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
